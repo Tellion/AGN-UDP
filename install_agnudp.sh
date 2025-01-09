a0001555c53593b7b8374ee5e476ef4976247d3b
Tellion <92380800+Tellion@users.noreply.github.com>
2025-01-09 22:04:15 +0000

    Update install_agnudp.sh

--- install_agnudp.sh
+++ install_agnudp.sh
@@ -22,10 +22,10 @@ PROTOCOL="udp"
 UDP_PORT=":36712"
 
 # OBFS
-OBFS="agnudp"
+OBFS="teslassh"
 
 # PASSWORDS
-PASSWORD="agnudp"
+PASSWORD="teslassh"
 
 # Basename of this script
 SCRIPT_NAME="$(basename "$0")"
