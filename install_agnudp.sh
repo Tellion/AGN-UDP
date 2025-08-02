fa2e6734ee8c52becde474f694aa3eee943825d2
Tellion <92380800+Tellion@users.noreply.github.com>
2025-08-02 23:54:14 +0000

    Update install_agnudp.sh

--- install_agnudp.sh
+++ install_agnudp.sh
@@ -22,10 +22,10 @@ PROTOCOL="udp"
 UDP_PORT=":36712"
 
 # OBFS
-OBFS="hone"
+OBFS="momo"
 
 # PASSWORDS
-PASSWORD="0000"
+PASSWORD="momo1"
 
 # Basename of this script
 SCRIPT_NAME="$(basename "$0")"
